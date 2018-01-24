e8dd240e847ef580ceee2f8ff80212f1db2bf519
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-01-24 16:57:55 +0530

    mul.C

---
+++ mul.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,a;
+	printf("\nthe num u want fr the fr the fist :");
+	scanf("%d",&a);
+for(i=5;i<=a;i++)	
+{
+	i=i*a;
+	printf("\nNums:%d",i);
+}
+
+	return 0;
+}
