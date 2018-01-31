04551af4eccf9047c9fb2ca6e3c0c0d974253582
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-01-31 16:59:46 +0530

    prime2.C

---
+++ prime2.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+ long int a,d,s,i;
+printf("enter the num");
+scanf("%ld",&d);
+printf("enter the num");
+scanf("%ld",&s);
+for(i=d;i<=s;i++)
+{
+printf("\n%d",i)
+}
+return 0;
+}
