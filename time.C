44b2a38f23ff29a783fedfc6c9e9debb3237a4af
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-02-05 16:29:41 +0530

    time.C

---
+++ time.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) 
+{
+float a,s,d;
+printf("\nEnter the  initial time:");
+scanf("%.2f",&a);
+printf("\nEnter the  final time:");
+scanf("%.2f",&s);
+d=a+s;
+printf("\n the total hour is:%.2f",d);
+return 0;
+}
