7f9a8136b0d26a549640d8a761afe9bc22cc3954
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-02-07 16:42:08 +0530

    arr.C

--- arr.C
+++ arr.C
@@ -1,15 +1,14 @@
 #include<stdio.h>
-int main()
 {
 long int arr[50];
 int i,n,a;
 printf("enter size of  array:");
 scanf("%d",&n);
-printf("enter %d elements in the array:" ,n);
-scanf("%d",&a);
-for(i=0;i<a;i++)
+//printf("enter %d elements in the array:" ,n);
+//scanf("%d",&a);
+for(i=0;i<n;i++)
 {
-for(i=0;i<a;i++)
+for(i=0;i<n;i++)
 {
 scanf("%ld",arr[i]);
 printf("\n elements in array are:%ld",arr[i]);
