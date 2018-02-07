d5571fdb94a793b0dd31016214e1d7aeafb54dfc
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-02-07 16:48:40 +0530

    str.C

---
+++ str.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int n;
+printf("enter a string");
+scanf("%d",&n);
+if(n>0 && n<10000)
+{
+	printf("\nyes");
+}
+else
+{
+	printf("\n no");
+}
+	return 0;
+}
