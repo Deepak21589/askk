cec8c1781954741e6b3f10de97f964ca70761a74
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-02-09 16:42:11 +0530

    g.C

--- g.C
+++ g.C
@@ -2,9 +2,10 @@
 int main()
 {
     int n,m,i,hcf;
-    printf("\nenter the array length%d",n);
-    printf("\n enter the array elements%d",m)
-    scanf("%d %d",&n,&m);
+    printf("\nenter the array length");
+    scanf("%d",&n);
+    printf("\n enter the array elements");
+    scanf("%d",&m);
     for(i=1;i<=n && i<=m;i++)
     {
         if(n%i==0 && m%i==0)
