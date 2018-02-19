02271119fd80b05e35e7988ebee5a58a4d8ac071
Deepak21589 <35220617+Deepak21589@users.noreply.github.com>
2018-02-19 16:26:15 +0530

    str3.C

---
+++ str3.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+	char str[50];
+	int j,co=0;
+	printf("enter the sentence:");
+	scanf("%s",str);
+	for(j=0;str[j]!='\0';j++)
+	{
+		co=co+1;
+	}
+	for(j=0;str[j]!='\0';j++)
+	{
+		if(str[j]==' ')
+		{
+			co=co-1;
+		}
+	}
+	printf("\nthe number of characters in the line are:%d",co);
+	return 0;
+}
